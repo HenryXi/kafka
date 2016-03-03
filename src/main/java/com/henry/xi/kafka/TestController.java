fd85c32bb64bbd869130c75d580877cba06c95a2
yong <xxy668@foxmail.com>
2016-03-03 20:57:13 +0800

    init develop branch

---
+++ src/main/java/com/henry/xi/kafka/TestController.java
@@ -0,0 +1,14 @@
+package com.henry.xi.kafka;
+
+//@Controller
+public class TestController {
+//    @Autowired
+//    private MessageExecutor messageExecutor;
+//
+//    @RequestMapping(value = "/test")
+//    @ResponseBody
+//    public String test(){
+//        String test = messageExecutor.toString();
+//        return new Message("a","luck").toString();
+//    }
+}
